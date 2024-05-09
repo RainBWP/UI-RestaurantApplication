545195ed867f424f075ef71ee8770a4006219b63
RainBWP <zeusxd952@gmail.com>
2024-05-09 03:31:40 -0600

    Nuevo Procesar Pago, ahora si esta centrado :)

--- src/ProcesarPago.vue
+++ src/ProcesarPago.vue
@@ -3,29 +3,25 @@
 
 <template>
     <div class="pagoScreen">
-        <h1>Procesando Pago...</h1>
+        <h2>Procesando Pago...</h2>
     </div>
     
 </template>
 
 <style scoped>
-    .pagoScreen{
+    .pagoScreen {
+        width: 90%;
         position: absolute;
-        top: 25%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
         z-index: 12;
-
         max-width: 90vw;
-
         text-align: center;
-        margin: 5%;
         padding: 5%;
-        display: flex;
-        align-items: center;
-        align-self: center;
-        flex-direction: column;
         box-shadow: 10px 10px 10px #0005;
         border-radius: 5vw;
+        background-color: var(--color-text-element-background);
+        }
 
-        background-color: var(--color-text-element-background)
-    }
 </style>
